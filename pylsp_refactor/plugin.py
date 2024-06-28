a85afee6900536a136e68513089996a45d5d65dc
zibertscrem <zibertscrem@gmail.com>
2024-06-28 07:08:57 +0300

    Make plugin config enable or disable etire plugin

--- pylsp_refactor/plugin.py
+++ pylsp_refactor/plugin.py
@@ -29,6 +29,8 @@ def pylsp_commands(
     config: Any,
     workspace: Workspace,  # noqa: ARG001
 ) -> list[str]:
+    if is_plugin_disabled(config):
+        return []
     return code_actions.commands(config)
 
 
@@ -41,6 +43,8 @@ def pylsp_code_actions(
     range: dict[str, Any],  # noqa: A002
     context: dict[str, Any],
 ) -> list[dict[str, Any]]:
+    if is_plugin_disabled(config):
+        return []
     logger.info("textDocument/codeAction: %s %s %s", document, range, context)
     range_ = utils.parse_range(range)
     response = []
@@ -56,6 +60,8 @@ def pylsp_execute_command(
     command: str,
     arguments: tuple[str, dict[str, Any], ...],
 ) -> None:
+    if is_plugin_disabled(config):
+        return
     logger.info("workspace/executeCommand: %s %s", command, arguments)
     if command in code_actions.commands(config):
         doc_uri = arguments[0]
@@ -63,3 +69,8 @@ def pylsp_execute_command(
         document = workspace.get_document(doc_uri)
         range_ = utils.parse_range(input_range)
         code_actions.apply(config, workspace, document, range_, command, arguments)
+
+
+def is_plugin_disabled(config: Any) -> bool:
+    cfg = config.plugin_settings("pylsp_refactor")
+    return not cfg.get("enabled", False)
